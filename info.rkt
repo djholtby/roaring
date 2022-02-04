8836822b3b72b8376dd43a8de399f044e5acbe67
Dan Holtby <djholtby@gmail.com>
2022-02-04 11:49:15 -0500

    change description in info.rkt

--- info.rkt
+++ info.rkt
@@ -3,7 +3,7 @@
 (define deps '("base"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/roaring.scrbl" ())))
-(define pkg-desc "Description Here")
+(define pkg-desc "FFI bindings for CRoaringBitmap")
 (define version "0.0")
 (define pkg-authors '(djholtby))
 (define license '(Apache-2.0 OR MIT))
